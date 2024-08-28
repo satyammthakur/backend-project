c77b5ff9924797cb2ca48911f62d5b85d6fd3c29
satyammthakur <satyammthakur@gmail.com>
2024-08-28 12:03:04 +0530

    Implemeting Pug Templates

--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,15 @@
 const path = require('path');
+
+const express = require('express');
+
 const rootDir = require('../util/path');
-const express=require("express");
+const adminData = require('./admin');
 
 const router = express.Router();
 
-
-
-router.get('/',(req,res,next)=>{
-    res.sendFile(path.join(rootDir, 'views' ,'shop.html'))
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {prods: products, docTitle: 'Shop'});
 });
 
-module.exports=router;
+module.exports = router;
