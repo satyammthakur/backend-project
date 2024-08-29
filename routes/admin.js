7ce6f53a8dcf209dab30eee49120def92bffe5c7
satyammthakur <satyammthakur@gmail.com>
2024-08-29 23:01:14 +0530

    Finishings The Pug Template

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,4 @@
+
 const path = require('path');
 
 const express = require('express');
@@ -10,13 +11,16 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+    });
 });
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
+    products.push({ title: req.body.title });
+    res.redirect('/');
 });
 
 exports.routes = router;
